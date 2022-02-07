94629c6bf30393f1463bba0ab7fea9651a0b043d
yaswanth <yaswanth@turingsystems.com>
2022-02-07 13:10:06 +0530

    Add project files.

---
+++ IfCondition/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 2;
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("even number");
+            }
+        }
+    }
+}
